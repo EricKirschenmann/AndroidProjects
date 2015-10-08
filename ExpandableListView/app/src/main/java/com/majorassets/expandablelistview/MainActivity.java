380d55662dca9561e732ee193f090aeaad20afad
EricKirschenmann <erickirschenmann92@gmail.com>
2015-10-08 11:34:18 -0700

    Move the prepareList method into the new class as well as create accessor methods to get the List and HashMap as well as update MainActivity to utilize them.

--- ExpandableListView/app/src/main/java/com/majorassets/expandablelistview/MainActivity.java
+++ ExpandableListView/app/src/main/java/com/majorassets/expandablelistview/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
+    List<String> listDataHeader = new ArrayList<String>();;
+    HashMap<String, List<String>> listDataChild = new HashMap<String, List<String>>();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
         // preparing list data
-        prepareListData();
+        //prepareListData();
+        CreateList createList = new CreateList();
+
+        listDataHeader = createList.getListDataHeader();
+        listDataChild = createList.getHashMap();
 
         listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
@@ -71,79 +74,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /*
-    * Preparing the list data
-    * TODO: Move into it's own class file to make it more modular and get it out of the main Activity
-    */
-    private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        listDataHeader.add("Entertainment");
-        listDataHeader.add("Fashion");
-        listDataHeader.add("Food");
-        listDataHeader.add("Hobbies");
-        listDataHeader.add("Medical");
-        listDataHeader.add("Other");
-
-        // Adding child data
-        List<String> food = new ArrayList<String>();
-        food.add("Dishes");
-        food.add("Drinks");
-        food.add("Restaurants");
-        food.add("Snacks");
-
-        Collections.sort(food);
-
-        List<String> entertainment = new ArrayList<String>();
-        entertainment.add("Books");
-        entertainment.add("Games");
-        entertainment.add("Movies");
-        entertainment.add("TV Shows");
-        entertainment.add("Music");
-        entertainment.add("Theater");
-
-        Collections.sort(entertainment);
-
-        List<String> fashion = new ArrayList<String>();
-        fashion.add("Clothing");
-        fashion.add("Shoes");
-        fashion.add("Accessories");
-        fashion.add("Jewelry");
-
-        Collections.sort(fashion);
-
-        List<String> hobbies = new ArrayList<String>();
-        hobbies.add("Indoor");
-        hobbies.add("Outdoor");
-        hobbies.add("Sports");
-
-        Collections.sort(hobbies);
-
-        List<String> medical = new ArrayList<String>();
-        medical.add("Allergies");
-        medical.add("Illnesses");
-        medical.add("Phobias");
-        medical.add("Medication");
-
-        Collections.sort(medical);
-
-        List<String> other = new ArrayList<String>();
-        other.add("Animals");
-        other.add("Colors");
-        other.add("Add Your Own");
-
-        Collections.sort(other);
-
-
-        listDataChild.put(listDataHeader.get(0), entertainment); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), fashion);
-        listDataChild.put(listDataHeader.get(2), food);
-        listDataChild.put(listDataHeader.get(3), hobbies);
-        listDataChild.put(listDataHeader.get(4), medical);
-        listDataChild.put(listDataHeader.get(5), other);
     }
 }
