5382e0ca8ad02d5796aa49577fe15658716ddcad
dillon-blanksma <dgblanks@gmail.com>
2016-02-26 17:02:40 -0800

    working app

--- BetterHalf/app/src/main/java/com/majorassets/betterhalf/HomeActivity.java
+++ BetterHalf/app/src/main/java/com/majorassets/betterhalf/HomeActivity.java
@@ -2,14 +2,12 @@ package com.majorassets.betterhalf;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
@@ -27,16 +25,6 @@ public class HomeActivity extends AppCompatActivity
 		setContentView(R.layout.activity_home);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
 
 		ref = db.getInstance();
 
