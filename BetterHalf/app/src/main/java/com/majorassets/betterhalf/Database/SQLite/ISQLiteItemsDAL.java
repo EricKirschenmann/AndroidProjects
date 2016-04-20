3081ab2ee425c85e93b9decb99fcf46c3e81a12d
marissamiller11 <marissamiller11@apu.edu>
2016-04-20 10:42:16 -0700

    Created add, update and delete statements to ItemDAL
    Have not been able to test them yet.

--- BetterHalf/app/src/main/java/com/majorassets/betterhalf/Database/SQLite/ISQLiteItemsDAL.java
+++ BetterHalf/app/src/main/java/com/majorassets/betterhalf/Database/SQLite/ISQLiteItemsDAL.java
@@ -6,7 +6,7 @@ import com.majorassets.betterhalf.Model.BaseDataItem;
  * Created by Marissa on 4/20/2016.
  */
 public interface ISQLiteItemsDAL {
-    void addItem(BaseDataItem item);
-    void updateItem(BaseDataItem item);
-    BaseDataItem deleteItem(BaseDataItem item);
+    void addItem(BaseDataItem item, String tableName, String colUUID, String colLabel, String colValue);
+    void updateItem(BaseDataItem item, String tableName, String colUUID, String colLabel, String colValue);
+    BaseDataItem deleteItem(BaseDataItem item, String tableName, String colUUID);
 }
