4217ad6896424829d1721d146713559805fd0f22
dillon-blanksma <dgblanks@gmail.com>
2016-04-21 20:19:38 -0700

    some of Marissa's sqlite stuff

---
+++ BetterHalf/app/src/main/java/com/majorassets/betterhalf/Database/SQLite/DataDBSchema.java
@@ -0,0 +1,262 @@
+package com.majorassets.betterhalf.Database.SQLite;
+
+/**
+ * Created by Marissa on 4/11/2016.
+ *
+ * Contains the schemas for each of the data item tables.
+ */
+public class DataDBSchema {
+
+    /*  Entertainment Tables */
+    public static final class BooksTable
+    {
+        public static final String NAME = "Books";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class GamesTable
+    {
+        public static final String NAME = "Games";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class MoviesTable
+    {
+        public static final String NAME = "Movies";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class MusicTable
+    {
+        public static final String NAME = "Music";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class TheaterTable
+    {
+        public static final String NAME = "Theater";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class TVShowsTable
+    {
+        public static final String NAME = "TVShows";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+
+    /*  Fashion */
+    public static final class AccessoriesTable
+    {
+        public static final String NAME = "Accessories";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class ClothingTable
+    {
+        public static final String NAME = "Clothing";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class JewelryTable
+    {
+        public static final String NAME = "Jewelry";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class ShoesTable
+    {
+        public static final String NAME = "Shoes";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+
+    /*  Food */
+    public static final class DrinksTable
+    {
+        public static final String NAME = "Drinks";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class EntreesTable
+    {
+        public static final String NAME = "Entrees";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class RestaurantsTable
+    {
+        public static final String NAME = "Restaurants";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class SidesTable
+    {
+        public static final String NAME = "Sides";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class SnacksTable
+    {
+        public static final String NAME = "Snacks";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+
+    /*  Hobbies */
+    public static final class IndoorTable
+    {
+        public static final String NAME = "Indoor";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class OutdoorTable
+    {
+        public static final String NAME = "Outdoor";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class SportsTable
+    {
+        public static final String NAME = "Sports";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+
+    /*  Medical */
+    public static final class AllergiesTable
+    {
+        public static final String NAME = "Allergies";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class IllnessesTable
+    {
+        public static final String NAME = "Illnesses";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class MedicationTable
+    {
+        public static final String NAME = "Medication";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+    public static final class PhobiasTable
+    {
+        public static final String NAME = "Phobias";
+
+        public static final class Cols
+        {
+            public static final String LABEL = "label";
+            public static final String VALUE = "value";
+        }
+    }
+
+}
+
