96d08f463e105e817699f3775ab500646989aec2
Eric Kirschenmann <erickirschenmann92@gmail.com>
2016-04-22 22:30:51 -0700

    Add method that instantiates and fills an array of strings with the page titles. Move the FAB into the fragment so you can add by specific category (todo). Add onItemClickListener to the listviews so you can click and search an item.

--- BetterHalf/app/src/main/java/com/majorassets/betterhalf/DataItemController/DataItemActivityFragment.java
+++ BetterHalf/app/src/main/java/com/majorassets/betterhalf/DataItemController/DataItemActivityFragment.java
@@ -1,15 +1,19 @@
 package com.majorassets.betterhalf.DataItemController;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.majorassets.betterhalf.R;
+import com.majorassets.betterhalf.SingleItemEditActivity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,10 +24,15 @@ import java.util.HashMap;
  */
 public class DataItemActivityFragment extends Fragment
 {
+
+    //TODO: Find out why onCreateView runs twice whenever the activity is opened
+
     private ArrayList<String> Array = new ArrayList<String>();
     public HashMap stuffs = new HashMap();
     private DataItemPagerAdapter mDataItemPagerAdapter;
     private ListView listView;
+    private FloatingActionButton mAddItemFab;
+    private String[] titles;
 
 	public static final String ARG_PAGE = "com.majorassets.betterhalf.page";
 
@@ -59,12 +68,36 @@ public class DataItemActivityFragment extends Fragment
         //SAMPLE DATA
 
         //DECLARE ADAPTER FOR LISTVIEW
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_expandable_list_item_1, Array);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_expandable_list_item_1, Array);
         listView = (ListView) view.findViewById(android.R.id.text1);
         listView.setAdapter(adapter);
 
         mDataItemPagerAdapter = new DataItemPagerAdapter(getFragmentManager());
         Bundle args = getArguments();
+        //store all the titles into an array
+        getTitles();
+
+        //get reference to floating action button in bottom right of screen
+        mAddItemFab = (FloatingActionButton) getActivity().findViewById(R.id.fab_add_item);
+        mAddItemFab.setOnClickListener(new View.OnClickListener()
+        {
+            //have the click start the edit screen for a single item
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getContext(), SingleItemEditActivity.class);
+                //intent.putExtra(Intent.EXTRA_REFERRER, titles[]);
+                startActivity(intent);
+            }
+        });
+
+        //Add listener for the click on a list item to search the web
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String query = adapter.getItem(position);
+            }
+        });
 
         //DECLARE VARIABLES FOR LISTVIEW
         String keyString = null;
@@ -90,5 +123,18 @@ public class DataItemActivityFragment extends Fragment
 
         return view;
 	}
+
+    private void getTitles() {
+        titles = new String[mDataItemPagerAdapter.getCount()];
+
+        for(int i = 0; i < mDataItemPagerAdapter.getCount(); i++) {
+            titles[i] = mDataItemPagerAdapter.getPageTitle(i).toString();
+        }
+    }
+
+    private void searchWeb(String query) {
+
+    }
+
 }
 
